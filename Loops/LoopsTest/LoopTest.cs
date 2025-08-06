0acf64008dc4c835620d9ce2b96f357cb0e541f1
FRagIC17 <FRagIC17@users.noreply.github.com>
2025-08-06 12:23:40 +0200

    created test unitTest

---
+++ Loops/LoopsTest/LoopTest.cs
@@ -0,0 +1,18 @@
+namespace LoopsTest
+{
+    public class LoopTest
+    {
+        [Fact]
+        public void ToThePowerOfNextNumberExpectedOutput()
+        {
+            // Arrange
+            int expected = ;
+
+            // Act
+            int actual = Loops.Program.ToThePowerOfNextNumber(-2, 3);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
