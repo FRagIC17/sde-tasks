868f3fe6b189fee5a95c929577cfea5e0293f2a1
FRagIC17 <FRagIC17@users.noreply.github.com>
2025-08-18 14:28:00 +0200

    Interface tasks

---
+++ Interface/IPrintable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal interface IPrintable
+    {
+        void Print();
+    }
+
+    public class Invoice : IPrintable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing Invoice...");
+        }
+    }
+
+    public class Report : IPrintable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing Report...");
+        }
+    }
+
+    public class Letter : IPrintable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing Letter...");
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCardPayment creditCardPayment = new CreditCardPayment();
+            PayPalPayment payPalPayment = new PayPalPayment();
+
+            creditCardPayment.ProcessPayment(100.00m);
+            payPalPayment.ProcessPayment(200.00m);
+
+
+            List<IPrintable> printables = new List<IPrintable>
+            {
+                new Invoice(),
+                new Report(),
+                new Letter()
+            };
+
+            foreach (var printable in printables)
+            {
+                printable.Print();
+            } 
+
+        }
+    }
+
+    public interface IPayment
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardPayment : IPayment
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing credit card payment of {amount} kr");
+        }
+    }
+
+    public class PayPalPayment : IPayment
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing PayPal payment of {amount} kr");
+        }
+    }
+}
